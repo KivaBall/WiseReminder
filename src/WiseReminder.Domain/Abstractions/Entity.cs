fe20a85f46579cd64e1685aded5d846644372b5d
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 01:52:36 +0200

    Add abstract entity for all future entities

---
+++ src/WiseReminder.Domain/Abstractions/Entity.cs
@@ -0,0 +1,20 @@
+﻿namespace WiseReminder.Domain.Abstractions;
+
+public abstract class Entity
+{
+    internal Entity()
+    {
+        Id = Guid.NewGuid();
+        AddedAt = DateTime.Now;
+    }
+
+    public Guid Id { get; }
+    public DateTime AddedAt { get; }
+    public DateTime? DeletedAt { get; private set; }
+
+    internal Entity DeleteEntity()
+    {
+        DeletedAt = DateTime.Now;
+        return this;
+    }
+}
