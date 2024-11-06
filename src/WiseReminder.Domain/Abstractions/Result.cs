6806c98b8191f031ad8c0181af548edf180dcc8c
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 01:54:54 +0200

    Add result class

---
+++ src/WiseReminder.Domain/Abstractions/Result.cs
@@ -0,0 +1,32 @@
+﻿namespace WiseReminder.Domain.Abstractions;
+
+public class Result(bool isSuccess, Error error)
+{
+    public bool IsSuccess { get; } = isSuccess;
+    public Error Error { get; } = error;
+
+    public static Result Success()
+    {
+        return new Result(true, Error.None);
+    }
+
+    public static Result Failure(Error error)
+    {
+        return new Result(false, error);
+    }
+}
+
+public sealed class Result<TEntity>(bool isSuccess, Error error, TEntity? entity) : Result(isSuccess, error)
+{
+    public TEntity? Entity { get; } = entity;
+
+    public static Result<T> Success<T>(T entity)
+    {
+        return new Result<T>(true, Error.None, entity);
+    }
+
+    public static Result<T> Failure<T>(T? entity, Error error)
+    {
+        return new Result<T>(false, error, entity);
+    }
+}
