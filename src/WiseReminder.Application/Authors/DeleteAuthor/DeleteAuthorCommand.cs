cec39245d09beb5621725f7d72d09d289b66cadd
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 05:24:04 +0200

    Add CUD operations for authors

---
+++ src/WiseReminder.Application/Authors/DeleteAuthor/DeleteAuthorCommand.cs
@@ -0,0 +1,7 @@
+﻿using WiseReminder.Application.Abstractions.MediatR;
+using WiseReminder.Application.Categories.DeleteCategory;
+using WiseReminder.Domain.Categories;
+
+namespace WiseReminder.Application.Authors.DeleteAuthor;
+
+public sealed record DeleteAuthorCommand(Guid Id) : ICommand;
