c098584e241492f3fc4488c5f90961e0bbda9448
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 04:52:55 +0200

    Add GetAllCategoriesQueryHandler

---
+++ src/WiseReminder.Application/Categories/GetAllCategories/GetAllCategoriesQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using WiseReminder.Application.Abstractions.MediatR;
+using WiseReminder.Domain.Abstractions;
+using WiseReminder.Domain.Categories;
+
+namespace WiseReminder.Application.Categories.GetAllCategories;
+
+public sealed class GetAllCategoriesQueryHandler(ICategoryRepository categoryRepository, IMapper mapper)
+    : IQueryHandler<GetAllCategoriesQuery, ICollection<CategoryVm>>
+{
+    private readonly ICategoryRepository _categoryRepository = categoryRepository;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<Result<ICollection<CategoryVm>>> Handle(GetAllCategoriesQuery request,
+        CancellationToken cancellationToken)
+    {
+        var categories = await _categoryRepository.GetAllCategories();
+
+        return Result<ICollection<CategoryVm>>.Success(_mapper.Map<ICollection<CategoryVm>>(categories));
+    }
+}
