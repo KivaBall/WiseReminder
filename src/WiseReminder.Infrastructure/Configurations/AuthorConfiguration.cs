65251b2efc70851a29478028db520ea649e65cd4
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 13:18:38 +0200

    Modify AuthorConfiguration

--- src/WiseReminder.Infrastructure/Configurations/AuthorConfiguration.cs
+++ src/WiseReminder.Infrastructure/Configurations/AuthorConfiguration.cs
@@ -8,8 +8,7 @@ public sealed class AuthorConfiguration : IEntityTypeConfiguration<Author>
 {
     public void Configure(EntityTypeBuilder<Author> builder)
     {
-        builder.HasQueryFilter(author => !(author.DeletedAt == null));
-        builder.HasIndex(author => author.DeletedAt == null);
+        builder.HasQueryFilter(author => author.DeletedAt == null);
 
         builder.Property(author => author.Name)
             .HasMaxLength(64)
