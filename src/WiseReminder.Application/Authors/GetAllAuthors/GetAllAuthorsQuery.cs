56fff4045702f8476d1a35fb86a94caf0036fdbe
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 05:24:30 +0200

    Add R operations for authors

---
+++ src/WiseReminder.Application/Authors/GetAllAuthors/GetAllAuthorsQuery.cs
@@ -0,0 +1,8 @@
+﻿using WiseReminder.Application.Abstractions.MediatR;
+using WiseReminder.Application.Categories.GetAllCategories;
+using WiseReminder.Application.Categories;
+using WiseReminder.Domain.Categories;
+
+namespace WiseReminder.Application.Authors.GetAllAuthors;
+
+public sealed record GetAllAuthorsQuery : IQuery<ICollection<AuthorVm>>;
---
+++ src/WiseReminder.Application/Authors/GetAuthorById/GetAuthorByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using WiseReminder.Application.Abstractions.MediatR;
+using WiseReminder.Application.Categories.GetCategoryById;
+using WiseReminder.Application.Categories;
+using WiseReminder.Domain.Categories;
+
+namespace WiseReminder.Application.Authors.GetAuthorById;
+
+public sealed record GetAuthorByIdQuery(Guid Id) : IQuery<AuthorVm>;
