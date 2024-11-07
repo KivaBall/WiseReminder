7b0a6888437d25554236e2428946f4ad4fc63697
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 05:58:05 +0200

    Add CUD operations for quotes

---
+++ src/WiseReminder.Application/Quotes/CreateQuote/CreateQuoteCommand.cs
@@ -0,0 +1,6 @@
+﻿using WiseReminder.Application.Abstractions.MediatR;
+using WiseReminder.Application.Authors.CreateAuthor;
+
+namespace WiseReminder.Application.Quotes.CreateQuote;
+
+public sealed record CreateQuoteCommand(string Text, Guid AuthorId, Guid CategoryId, DateOnly QuoteDate) : ICommand;
---
+++ src/WiseReminder.Application/Quotes/DeleteQuote/DeleteQuoteCommand.cs
@@ -0,0 +1,7 @@
+﻿using WiseReminder.Application.Abstractions.MediatR;
+using WiseReminder.Application.Authors.DeleteAuthor;
+using WiseReminder.Domain.Authors;
+
+namespace WiseReminder.Application.Quotes.DeleteQuote;
+
+public sealed record DeleteQuoteCommand(Guid Id) : ICommand;
