fc47ddbf772e83aa2484b81809b8a91bc85e1d51
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 02:17:26 +0200

    Add Category entity

---
+++ src/WiseReminder.Domain/Categories/Category.cs
@@ -0,0 +1,21 @@
+﻿using WiseReminder.Domain.Abstractions;
+
+namespace WiseReminder.Domain.Categories;
+
+public sealed class Category : Entity
+{
+    public Category(CategoryName name, CategoryDescription description)
+    {
+        Name = name;
+        Description = description;
+    }
+
+    private Category()
+    {
+
+    }
+
+    public CategoryName Name { get; internal set; }
+    public CategoryDescription Description { get; internal set; }
+    public ICollection<Quote> Quotes { get; } = [];
+}
