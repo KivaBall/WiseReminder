618e509ffc4c6e02e9df4f192e2b53deccfeadfc
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 04:48:52 +0200

    Amend Get method of repositories

--- src/WiseReminder.Domain/Categories/ICategoryRepository.cs
+++ src/WiseReminder.Domain/Categories/ICategoryRepository.cs
@@ -5,6 +5,6 @@ public interface ICategoryRepository
     void CreateCategory(Category category);
     void UpdateCategory(Category category);
     void DeleteCategory(Category category);
-    Task<Category> GetCategoryById(Guid id);
+    Task<Category?> GetCategoryById(Guid id);
     Task<ICollection<Category>> GetAllCategories();
 }
