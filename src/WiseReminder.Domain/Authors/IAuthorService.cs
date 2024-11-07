d902acfea6b8ee6c14ffd9ef16aa2500ca0ef204
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 02:58:12 +0200

    Add Author interfaces for repository and service

---
+++ src/WiseReminder.Domain/Authors/IAuthorService.cs
@@ -0,0 +1,12 @@
+﻿namespace WiseReminder.Domain.Authors;
+
+public interface IAuthorService
+{
+    Author CreateAuthor(AuthorName name, AuthorBiography biography, AuthorDateOfBirth dateOfBirth,
+        AuthorDateOfDeath dateOfDeath);
+
+    Author UpdateAuthor(Author author, AuthorName name, AuthorBiography biography, AuthorDateOfBirth dateOfBirth,
+        AuthorDateOfDeath dateOfDeath);
+
+    Author DeleteAuthor(Author author);
+}
