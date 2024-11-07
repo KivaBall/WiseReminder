e81edbfd88c6656f1d20c8b1d9d05b729d12ae72
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 09:15:57 +0200

    Add AppDbContext with IUnitOfWork pattern

---
+++ src/WiseReminder.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using WiseReminder.Domain.Abstractions;
+using WiseReminder.Domain.Authors;
+using WiseReminder.Domain.Categories;
+using WiseReminder.Domain.Quotes;
+
+namespace WiseReminder.Infrastructure.Data;
+
+public sealed class AppDbContext : DbContext, IUnitOfWork
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Quote> Quotes { get; set; }
+
+    public async Task<bool> SaveChangesAsync()
+    {
+        return await base.SaveChangesAsync() > 0;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
