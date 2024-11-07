065ac5462f0099c14209dabc0a5c2a64e739c0cb
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 04:11:40 +0200

    Create interfaces for CQRS

---
+++ src/WiseReminder.Application/Abstractions/MediatR/ICommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using WiseReminder.Domain.Abstractions;
+
+namespace WiseReminder.Application.Abstractions.MediatR;
+
+public interface ICommand : IRequest<Result>;
+
+public interface ICommand<TEntity> : IRequest<Result<TEntity>>;
