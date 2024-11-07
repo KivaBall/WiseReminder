3ac53a66eeb499f1acd5146b7f06ac8ecc0a7fa7
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 09:20:05 +0200

    Add QuoteRepository

---
+++ src/WiseReminder.Infrastructure/Repositories/QuoteRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WiseReminder.Domain.Quotes;
+using WiseReminder.Infrastructure.Data;
+
+namespace WiseReminder.Infrastructure.Repositories;
+
+public sealed class QuoteRepository(AppDbContext context, IQuoteService quoteService) : IQuoteRepository
+{
+    private readonly AppDbContext _context = context;
+    private readonly IQuoteService _quoteService = quoteService;
+
+    public void CreateQuote(Quote quote)
+    {
+        _context.Quotes.Add(quote);
+    }
+
+    public void UpdateQuote(Quote quote)
+    {
+        _context.Quotes.Update(quote);
+    }
+
+    public void DeleteQuote(Quote quote)
+    {
+        _quoteService.DeleteQuote(quote);
+        _context.Quotes.Update(quote);
+    }
+
+    public async Task<Quote?> GetQuoteById(Guid id)
+    {
+        return await _context.Quotes.FirstOrDefaultAsync(quote => quote.Id == id);
+    }
+
+    public async Task<ICollection<Quote>?> GetQuotesByCategoryId(Guid categoryId)
+    {
+        return await _context.Quotes.Where(quote => quote.CategoryId == categoryId).ToListAsync();
+    }
+
+    public async Task<ICollection<Quote>?> GetQuotesByAuthorId(Guid authorId)
+    {
+        return await _context.Quotes.Where(quote => quote.AuthorId == authorId).ToListAsync();
+    }
+}
