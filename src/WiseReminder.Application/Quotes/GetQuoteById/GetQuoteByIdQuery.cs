cc2c0970ee2735b7ac882859634cd539d19195a0
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 05:58:24 +0200

    Add R operations for quotes

---
+++ src/WiseReminder.Application/Quotes/GetQuoteById/GetQuoteByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using WiseReminder.Application.Abstractions.MediatR;
+using WiseReminder.Application.Authors.GetAuthorById;
+using WiseReminder.Application.Authors;
+using WiseReminder.Domain.Authors;
+
+namespace WiseReminder.Application.Quotes.GetQuoteById;
+
+public sealed record GetQuoteByIdQuery(Guid Id) : IQuery<QuoteVm>;
---
+++ src/WiseReminder.Application/Quotes/GetQuotesByAuthorId/GetQuotesByAuthorIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using WiseReminder.Application.Abstractions.MediatR;
+using WiseReminder.Application.Categories;
+using WiseReminder.Application.Quotes.GetQuoteById;
+
+namespace WiseReminder.Application.Quotes.GetQuotesByAuthorId;
+
+public sealed record GetQuotesByAuthorIdQuery(Guid AuthorId) : IQuery<ICollection<QuoteVm>>;
