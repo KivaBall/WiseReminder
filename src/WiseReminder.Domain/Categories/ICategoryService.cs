cc6cbc8aba2bbfc435ee1fec81c9c7d38882b5fc
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 02:17:54 +0200

    Add category interfaces for repository and service

---
+++ src/WiseReminder.Domain/Categories/ICategoryService.cs
@@ -0,0 +1,8 @@
+﻿namespace WiseReminder.Domain.Categories;
+
+public interface ICategoryService
+{
+    Category CreateCategory(CategoryName name, CategoryDescription description);
+    Category UpdateCategory(Category category, CategoryName name, CategoryDescription description);
+    Category DeleteCategory(Category category);
+}
