8ec8b248f6133a9644285eb7ec74d3d53f6e994f
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 12:44:26 +0200

    Add AuthorsController

---
+++ src/WiseReminder.WebAPI/Controllers/Authors/AuthorsController.cs
@@ -0,0 +1,68 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WiseReminder.Application.Authors.CreateAuthor;
+using WiseReminder.Application.Authors.DeleteAuthor;
+using WiseReminder.Application.Authors.GetAllAuthors;
+using WiseReminder.Application.Authors.GetAuthorById;
+using WiseReminder.Application.Authors.UpdateAuthor;
+
+namespace WiseReminder.WebAPI.Controllers.Authors;
+
+[ApiController]
+[Route("api/authors")]
+public sealed class AuthorsController(ISender sender) : ControllerBase
+{
+    private readonly ISender _sender = sender;
+
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateAuthor(CreateAuthorRequest request)
+    {
+        var command =
+            new CreateAuthorCommand(request.Name, request.Biography, request.DateOfBirth, request.DateOfDeath);
+
+        var result = await _sender.Send(command);
+
+        return result.IsSuccess ? Ok() : BadRequest();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAuthorById(Guid id)
+    {
+        var query = new GetAuthorByIdQuery(id);
+
+        var result = await _sender.Send(query);
+
+        return result.IsSuccess ? Ok(result.Entity) : NotFound(result);
+    }
+
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAllAuthors()
+    {
+        var query = new GetAllAuthorsQuery();
+
+        var result = await _sender.Send(query);
+
+        return Ok(result.Entity);
+    }
+
+    [HttpPut("update")]
+    public async Task<IActionResult> UpdateAuthor(UpdateAuthorRequest request)
+    {
+        var command = new UpdateAuthorCommand(request.Id, request.Name, request.Biography, request.DateOfBirth,
+            request.DateOfDeath);
+
+        var result = await _sender.Send(command);
+
+        return result.IsSuccess ? Ok() : BadRequest();
+    }
+
+    [HttpDelete("delete")]
+    public async Task<IActionResult> DeleteAuthor(Guid id)
+    {
+        var command = new DeleteAuthorCommand(id);
+
+        var result = await _sender.Send(command);
+
+        return result.IsSuccess ? Ok() : BadRequest();
+    }
+}
