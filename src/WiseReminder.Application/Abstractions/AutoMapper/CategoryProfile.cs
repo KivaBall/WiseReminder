89ddc10fd00ef3fe27935b74ce636442529895ea
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 04:11:08 +0200

    Add CategoryProfile class for AutoMapper

---
+++ src/WiseReminder.Application/Abstractions/AutoMapper/CategoryProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WiseReminder.Application.Categories;
+using WiseReminder.Domain.Categories;
+
+namespace WiseReminder.Application.Abstractions.AutoMapper;
+
+public sealed class CategoryProfile : Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<Category, CategoryVm>()
+            .ForCtorParam("Id", options => options.MapFrom(category => category.Id))
+            .ForCtorParam("Name", options => options.MapFrom(category => category.Name.Value))
+            .ForCtorParam("Description", options => options.MapFrom(category => category.Description.Value));
+    }
+}
