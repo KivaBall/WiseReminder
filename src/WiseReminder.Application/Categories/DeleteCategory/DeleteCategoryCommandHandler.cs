3189c0a398026fc77b5a04ab9e7b31ceaa8dd432
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 04:52:42 +0200

    Add DeleteCategoryCommandHandler

---
+++ src/WiseReminder.Application/Categories/DeleteCategory/DeleteCategoryCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using WiseReminder.Application.Abstractions.MediatR;
+using WiseReminder.Domain.Abstractions;
+using WiseReminder.Domain.Categories;
+
+namespace WiseReminder.Application.Categories.DeleteCategory;
+
+public sealed class DeleteCategoryCommandHandler(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork)
+    : ICommandHandler<DeleteCategoryCommand>
+{
+    private readonly ICategoryRepository _categoryRepository = categoryRepository;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+    public async Task<Result> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var category = await _categoryRepository.GetCategoryById(request.Id);
+
+        if (category == null) return Result.Failure(CategoryErrors.CategoryNotFound);
+
+        _categoryRepository.DeleteCategory(category);
+
+        return await _unitOfWork.SaveChangesAsync() ? Result.Success() : Result.Failure(Error.Database);
+    }
+}
