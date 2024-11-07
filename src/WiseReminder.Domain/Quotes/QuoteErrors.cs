3cdda72fa6ed2c27a7421c39161813123298ced6
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 05:56:23 +0200

    Add QuoteErrors

---
+++ src/WiseReminder.Domain/Quotes/QuoteErrors.cs
@@ -0,0 +1,8 @@
+﻿using WiseReminder.Domain.Abstractions;
+
+namespace WiseReminder.Domain.Quotes;
+
+public static class QuoteErrors
+{
+    public static Error QuoteNotFound => new Error("Quote error", "Quote by Id was not found");
+}
