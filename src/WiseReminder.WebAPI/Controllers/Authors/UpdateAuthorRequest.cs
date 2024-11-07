2e603cd820dfd7dfdc39b7e6bfb27498e619ce53
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 12:43:00 +0200

    Add C, D author operation requests for controller

---
+++ src/WiseReminder.WebAPI/Controllers/Authors/UpdateAuthorRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace WiseReminder.WebAPI.Controllers.Authors;
+
+public sealed record UpdateAuthorRequest(
+    Guid Id,
+    string Name,
+    string Biography,
+    DateOnly DateOfBirth,
+    DateOnly DateOfDeath);
