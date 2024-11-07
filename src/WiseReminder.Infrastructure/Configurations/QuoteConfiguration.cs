4a63e304d239b079249ab83b91d5d26abaa88b41
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 09:19:22 +0200

    Add QuoteConfiguration

---
+++ src/WiseReminder.Infrastructure/Configurations/QuoteConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WiseReminder.Domain.Quotes;
+
+namespace WiseReminder.Infrastructure.Configurations;
+
+public sealed class QuoteConfiguration : IEntityTypeConfiguration<Quote>
+{
+    public void Configure(EntityTypeBuilder<Quote> builder)
+    {
+        builder.Property(author => author.Text)
+            .HasMaxLength(1024)
+            .HasConversion(text => text.Value, value => new QuoteText(value));
+
+        builder.Property(author => author.QuoteDate)
+            .HasConversion(quoteDate => quoteDate.Value, value => new QuoteDate(value));
+    }
+}
