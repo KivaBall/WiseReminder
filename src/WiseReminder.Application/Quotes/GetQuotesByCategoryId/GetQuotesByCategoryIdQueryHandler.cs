ce5cab03757a4b76f6458c02430912e21a414d69
Ihor Kobzar <supermensy@gmail.com>
2024-11-07 05:59:35 +0200

    Add GetQuotesByCategoryIdQueryHandler

---
+++ src/WiseReminder.Application/Quotes/GetQuotesByCategoryId/GetQuotesByCategoryIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using WiseReminder.Application.Abstractions.MediatR;
+using WiseReminder.Domain.Abstractions;
+using WiseReminder.Domain.Categories;
+using WiseReminder.Domain.Quotes;
+
+namespace WiseReminder.Application.Quotes.GetQoutesByCategoryId;
+
+public sealed class GetQuotesByCategoryIdQueryHandler(IQuoteRepository quoteRepository, IMapper mapper)
+    : IQueryHandler<GetQuotesByCategoryIdQuery, ICollection<QuoteVm>>
+{
+    private readonly IMapper _mapper = mapper;
+    private readonly IQuoteRepository _quoteRepository = quoteRepository;
+
+    public async Task<Result<ICollection<QuoteVm>>> Handle(GetQuotesByCategoryIdQuery request,
+        CancellationToken cancellationToken)
+    {
+        var quotes = await _quoteRepository.GetQuotesByCategoryId(request.CategoryId);
+
+        if (quotes == null)
+            return Result<ICollection<QuoteVm>>.Failure<ICollection<QuoteVm>>(null, CategoryErrors.CategoryNotFound);
+
+        return Result<ICollection<QuoteVm>>.Success(_mapper.Map<ICollection<QuoteVm>>(quotes));
+    }
+}
