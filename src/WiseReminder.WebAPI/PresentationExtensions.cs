2f84bbfc6f0380996b04488444c3b5d7f102a64c
Ihor Kobzar <supermensy@gmail.com>
2024-11-30 18:24:34 +0200

    Move some services to another static class, remove usings and remove cookie middleware

---
+++ src/WiseReminder.WebAPI/PresentationExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace WiseReminder.WebAPI;
+
+public static class PresentationExtensions
+{
+    public static IServiceCollection AddPresentationServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddControllers();
+        services.AddFluentValidationAutoValidation();
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddOpenApi();
+        services.AddAuth(configuration);
+
+        return services;
+    }
+
+    private static void AddAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "WiseReminder.com",
+                    ValidAudience = "WiseReminder.com",
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(configuration["JWTPassword"] ??
+                                               throw new Exception("JWTPassword isn't in appsettings.json")))
+                };
+            });
+        services.AddAuthorization();
+    }
+}
--- src/WiseReminder.WebAPI/Program.cs
+++ src/WiseReminder.WebAPI/Program.cs
@@ -1,59 +1,21 @@
-//TODO: Create GlobalUsings.cs
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using WiseReminder.Application;
-using WiseReminder.Infrastructure;
-using WiseReminder.WebAPI.Middlewares;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-//TODO: Move to another class 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = "WiseReminder.com",
-            ValidAudience = "WiseReminder.com",
-            IssuerSigningKey =
-                new SymmetricSecurityKey(
-                    "ThisIsSpecialSecurityKeyThisIsSpecialSecurityKeyThisIsSpecialSecurityKeyThisIsSpecialSecurityKey"u8
-                        .ToArray())
-        };
-    });
-
-builder.Services.AddAuthorization();
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddPresentationServices(builder.Configuration);
 
 var app = builder.Build();
 
 app.ApplyMigrations();
 
-//TODO: Replace
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
-
-//TODO: Remove Middleware (why isn't stateless?)
-app.UseMiddleware<JwtMiddleware>();
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
