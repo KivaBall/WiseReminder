af7df26d5e9e61cd17b137966d17cc969db3e045
Ihor Kobzar <supermensy@gmail.com>
2024-11-30 18:21:45 +0200

    Rebuild AdminController (into AuthorizationController)

---
+++ src/WiseReminder.WebAPI/Controllers/Authorization/SignInRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace WiseReminder.WebAPI.Controllers.Authorization;
+
+public sealed record SignInRequest
+{
+    public string? Login { get; init; }
+    public string? Password { get; init; }
+}
