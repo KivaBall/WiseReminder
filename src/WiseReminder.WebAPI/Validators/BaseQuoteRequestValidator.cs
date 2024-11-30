86e3e1caabbef5d68b3a6fc9af1bce1b3ad5754e
Ihor Kobzar <supermensy@gmail.com>
2024-11-30 18:22:55 +0200

    Add quote validators

---
+++ src/WiseReminder.WebAPI/Validators/BaseQuoteRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿namespace WiseReminder.WebAPI.Validators;
+
+public sealed class BaseQuoteRequestValidator : AbstractValidator<BaseQuoteRequest>
+{
+    public BaseQuoteRequestValidator()
+    {
+        RuleFor(q => q.Text)
+            .NotNull().WithMessage("Text must not be null")
+            .NotEmpty().WithMessage("Text must not be empty");
+
+        RuleFor(q => q.AuthorId)
+            .NotNull().WithMessage("Author ID must not be null");
+
+        RuleFor(q => q.CategoryId)
+            .NotNull().WithMessage("Category ID must not be null");
+
+        RuleFor(q => q.QuoteDate)
+            .NotNull().WithMessage("Quote date must not be null");
+    }
+}
