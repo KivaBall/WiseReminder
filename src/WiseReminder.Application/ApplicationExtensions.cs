c3d043589d22ce9729a5871156b45f9587f01499
Ihor Kobzar <supermensy@gmail.com>
2024-12-01 03:06:52 +0200

    Remove usings and AutoMapper in extensions for application layer

--- src/WiseReminder.Application/ApplicationExtensions.cs
+++ src/WiseReminder.Application/ApplicationExtensions.cs
@@ -1,18 +1,9 @@
-﻿using System.Reflection;
-using Microsoft.Extensions.DependencyInjection;
-using WiseReminder.Domain.Authors;
-using WiseReminder.Domain.Categories;
-using WiseReminder.Domain.Quotes;
-
-namespace WiseReminder.Application;
+﻿namespace WiseReminder.Application;
 
 public static class ApplicationExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        //TODO: Replace AutoMapper (obsolete)
-        services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
         services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 
         services.AddScoped<ICategoryService, CategoryService>();
