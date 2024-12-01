695b88c1142d46c773135baf1540e1202e8d85b6
Ihor Kobzar <supermensy@gmail.com>
2024-12-01 03:04:32 +0200

    Remake mappers for DTOs

---
+++ src/WiseReminder.Application/Abstractions/Mapping/AuthorToDtoMapper.cs
@@ -0,0 +1,16 @@
+﻿namespace WiseReminder.Application.Abstractions.Mapping;
+
+public static class AuthorToDtoMapper
+{
+    public static AuthorDto ToAuthorDto(this Author author)
+    {
+        return new AuthorDto
+        {
+            Id = author.Id,
+            Name = author.Name.Value,
+            Biography = author.Biography.Value,
+            DateOfBirth = author.DateOfBirth.Value,
+            DateOfDeath = author.DateOfDeath.Value
+        };
+    }
+}
