a39541b9774cad98959709a814f6915810e85f78
Ihor Kobzar <supermensy@gmail.com>
2024-12-01 03:02:30 +0200

    Remove unnecessary error

--- src/WiseReminder.Domain/Abstractions/Error.cs
+++ src/WiseReminder.Domain/Abstractions/Error.cs
@@ -3,6 +3,5 @@
 public sealed record Error(string Title, string Message)
 {
     public static Error None => new(string.Empty, string.Empty);
-    public static Error Unknown => new("Unknown error", "Received an unknown error in program");
     public static Error Database => new("Database error", "Something went wrong with database");
 }
