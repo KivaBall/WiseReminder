87b8b13d8ffc13e9d0fcd6fdf19f07fcb93b400b
Ihor Kobzar <supermensy@gmail.com>
2024-12-01 18:55:43 +0200

    Add interface for cache service

---
+++ src/WiseReminder.Application/Abstractions/Caching/ICacheService.cs
@@ -0,0 +1,7 @@
+﻿namespace WiseReminder.Application.Abstractions.Caching;
+
+public interface ICacheService
+{
+    Task CreateAsync<T>(string key, T entity) where T : class;
+    Task<T?> GetAsync<T>(string key) where T : class;
+}
