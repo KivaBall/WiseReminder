883ccb9d81b3258467e8abb12cc765e77e7e4c11
Ihor Kobzar <supermensy@gmail.com>
2024-12-07 23:22:46 +0200

    Amend commands for creating entities

--- src/WiseReminder.Application/Authors/CreateAuthor/CreateAuthorCommandHandler.cs
+++ src/WiseReminder.Application/Authors/CreateAuthor/CreateAuthorCommandHandler.cs
@@ -10,17 +10,25 @@ public sealed class CreateAuthorCommandHandler(
     private readonly IAuthorService _authorService = authorService;
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
-    public async Task<Result> Handle(CreateAuthorCommand request, CancellationToken cancellationToken)
+    public async Task<Result> Handle(
+        CreateAuthorCommand request,
+        CancellationToken cancellationToken)
     {
         var authorName = new AuthorName(request.Name);
         var authorBiography = new AuthorBiography(request.Biography);
         var authorDateOfBirth = new AuthorDateOfBirth(request.DateOfBirth);
         var authorDateOfDeath = new AuthorDateOfDeath(request.DateOfDeath);
 
-        var author = _authorService.CreateAuthor(authorName, authorBiography, authorDateOfBirth, authorDateOfDeath);
+        var author = _authorService.CreateAuthor(
+            authorName,
+            authorBiography,
+            authorDateOfBirth,
+            authorDateOfDeath);
 
         _authorRepository.CreateAuthor(author);
 
-        return await _unitOfWork.SaveChangesAsync() ? Result.Success() : Result.Failure(Error.Database);
+        return await _unitOfWork.SaveChangesAsync()
+            ? Result.Success()
+            : Result.Failure(Error.Database);
     }
 }
