7346d9dfd98a452e900cb0ef4db7e8642e772b72
Ihor Kobzar <supermensy@gmail.com>
2024-12-08 03:18:19 +0200

    Reimplement create quote method in service

--- src/WiseReminder.Domain/Quotes/IQuoteService.cs
+++ src/WiseReminder.Domain/Quotes/IQuoteService.cs
@@ -2,7 +2,7 @@
 
 public interface IQuoteService
 {
-    Quote CreateQuote(QuoteText text, Guid authorId, Author author, Guid categoryId, Category category,
+    Quote CreateQuote(QuoteText text, Guid authorId, Author? author, Guid categoryId, Category? category,
         QuoteDate quoteDate);
 
     Quote UpdateQuote(Quote quote, QuoteText text, Guid authorId, Author author, Guid categoryId, Category category,
