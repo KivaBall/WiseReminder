172adb357855640d79829ed6c092373f55f8ee01
Ihor Kobzar <supermensy@gmail.com>
2024-12-16 22:22:36 +0200

    Rebuild Quote class

--- src/WiseReminder.Domain/Quotes/Quote.cs
+++ src/WiseReminder.Domain/Quotes/Quote.cs
@@ -1,14 +1,13 @@
 ﻿namespace WiseReminder.Domain.Quotes;
 
-public sealed class Quote : Entity
+public sealed class Quote : Entity<Quote>
 {
-    internal Quote(QuoteText text, Guid authorId, Author author, Guid categoryId, Category category,
-        QuoteDate quoteDate)
+    private Quote(QuoteText text, Author author, Category category, Date quoteDate)
     {
         Text = text;
-        AuthorId = authorId;
+        AuthorId = author.Id;
         Author = author;
-        CategoryId = categoryId;
+        CategoryId = category.Id;
         Category = category;
         QuoteDate = quoteDate;
     }
@@ -18,10 +17,51 @@ public sealed class Quote : Entity
     {
     }
 
-    public QuoteText Text { get; internal set; }
-    public Guid AuthorId { get; internal set; }
-    public Author Author { get; internal set; }
-    public Guid CategoryId { get; internal set; }
-    public Category Category { get; internal set; }
-    public QuoteDate QuoteDate { get; internal set; }
+    public QuoteText Text { get; private set; }
+    public Date QuoteDate { get; private set; }
+
+    public Guid AuthorId { get; private set; }
+    public Author Author { get; private set; }
+    public Guid CategoryId { get; private set; }
+    public Category Category { get; private set; }
+
+    public static Result<Quote> Create(QuoteText text, Author author, Category category,
+        Date quoteDate)
+    {
+        if (!IsValidQuoteDate(author.BirthDate, author.DeathDate, quoteDate))
+        {
+            return Result.Fail("Invalid quote date");
+        }
+
+        var quote = new Quote(text, author, category, quoteDate);
+
+        return Result.Ok(quote);
+    }
+
+    public Result<Quote> Update(QuoteText text, Author author, Category category, Date quoteDate)
+    {
+        if (!IsValidQuoteDate(author.BirthDate, author.DeathDate, quoteDate))
+        {
+            return Result.Fail("Invalid quote date");
+        }
+
+        Text = text;
+        AuthorId = author.Id;
+        Author = author;
+        CategoryId = category.Id;
+        Category = category;
+        QuoteDate = quoteDate;
+
+        return Result.Ok(this);
+    }
+
+    private static bool IsValidQuoteDate(Date birthDate, Date? deathDate, Date quoteDate)
+    {
+        if (deathDate == null)
+        {
+            return birthDate.Year < quoteDate.Year;
+        }
+
+        return birthDate.Year < quoteDate.Year && deathDate.Year > quoteDate.Year;
+    }
 }
