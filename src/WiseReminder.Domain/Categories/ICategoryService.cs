fadf8a6d6e79325048dbcddb17fe90d85bd0c342
Ihor Kobzar <supermensy@gmail.com>
2024-12-16 22:14:15 +0200

    Remove all services for domain layer for rebuilding in the next commits

--- src/WiseReminder.Domain/Categories/ICategoryService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WiseReminder.Domain.Categories;
-
-public interface ICategoryService
-{
-    Category CreateCategory(CategoryName name, CategoryDescription description);
-    Category UpdateCategory(Category category, CategoryName name, CategoryDescription description);
-    Category DeleteCategory(Category category);
-}
