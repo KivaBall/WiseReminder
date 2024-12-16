0f58f7c980d0e05a2dbcd5da2185d59b0aecf11e
Ihor Kobzar <supermensy@gmail.com>
2024-12-16 22:19:38 +0200

    Rebuild Author class with factory and domain services

--- src/WiseReminder.Domain/Authors/Author.cs
+++ src/WiseReminder.Domain/Authors/Author.cs
@@ -1,14 +1,13 @@
 ﻿namespace WiseReminder.Domain.Authors;
 
-public sealed class Author : Entity
+public sealed class Author : Entity<Author>
 {
-    internal Author(AuthorName name, AuthorBiography biography, AuthorDateOfBirth dateOfBirth,
-        AuthorDateOfDeath dateOfDeath)
+    private Author(AuthorName name, AuthorBiography biography, Date birthDate, Date? deathDate)
     {
         Name = name;
         Biography = biography;
-        DateOfBirth = dateOfBirth;
-        DateOfDeath = dateOfDeath;
+        BirthDate = birthDate;
+        DeathDate = deathDate;
     }
 
     // ReSharper disable once UnusedMember.Local
@@ -16,9 +15,69 @@ public sealed class Author : Entity
     {
     }
 
-    public AuthorName Name { get; internal set; }
-    public AuthorBiography Biography { get; internal set; }
-    public AuthorDateOfBirth DateOfBirth { get; internal set; }
-    public AuthorDateOfDeath DateOfDeath { get; internal set; }
+    public AuthorName Name { get; private set; }
+    public AuthorBiography Biography { get; private set; }
+    public Date BirthDate { get; private set; }
+    public Date? DeathDate { get; private set; }
+
     public ICollection<Quote> Quotes { get; } = [];
+
+    public static Result<Author> Create(AuthorName name, AuthorBiography biography, Date birthDate,
+        Date? deathDate)
+    {
+        if (!IsValidDateDiapason(birthDate, deathDate))
+        {
+            return Result.Fail("Invalid date diapason between birth and death");
+        }
+
+        var author = new Author(name, biography, birthDate, deathDate);
+
+        return Result.Ok(author);
+    }
+
+    public Result<Author> Update(AuthorName name, AuthorBiography biography, Date birthDate,
+        Date? deathDate)
+    {
+        if (!IsValidDateDiapason(birthDate, deathDate))
+        {
+            return Result.Fail("Invalid date diapason between birth and death");
+        }
+
+        Name = name;
+        Biography = biography;
+        BirthDate = birthDate;
+        DeathDate = deathDate;
+
+        return Result.Ok(this);
+    }
+
+    private static bool IsValidDateDiapason(Date birthDate, Date? deathDate)
+    {
+        if (deathDate == null)
+        {
+            return true;
+        }
+
+        if (birthDate.Year > deathDate.Year)
+        {
+            return false;
+        }
+
+        if (birthDate.Year == deathDate.Year)
+        {
+            if (birthDate.Month > deathDate.Month)
+            {
+                return false;
+            }
+
+            if (birthDate.Month == deathDate.Month)
+            {
+                return birthDate.Day <= deathDate.Day;
+            }
+
+            return true;
+        }
+
+        return true;
+    }
 }
