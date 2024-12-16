62c29c6945506853c6e3a07423302d5af4b1a584
Ihor Kobzar <supermensy@gmail.com>
2024-12-16 22:18:43 +0200

    Rebuild value objects for Author class

--- src/WiseReminder.Domain/Authors/AuthorName.cs
+++ src/WiseReminder.Domain/Authors/AuthorName.cs
@@ -1,3 +1,11 @@
 ﻿namespace WiseReminder.Domain.Authors;
 
-public sealed record AuthorName(string Value);
+public sealed record AuthorName
+{
+    public AuthorName(string value)
+    {
+        Value = value;
+    }
+
+    public string Value { get; }
+}
