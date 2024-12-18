85d1711bdda0919ba66e783d82910c87ac4cc346
Ihor Kobzar <supermensy@gmail.com>
2024-12-18 03:19:30 +0200

    Amend parameters in applications interfaces

--- src/WiseReminder.Application/Abstractions/MediatR/ICommandHandler.cs
+++ src/WiseReminder.Application/Abstractions/MediatR/ICommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿namespace WiseReminder.Application.Abstractions.MediatR;
 
-public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand, Result> where TCommand : ICommand;
+public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand, Result>
+    where TCommand : ICommand;
 
 public interface ICommandHandler<in TCommand, TEntity> : IRequestHandler<TCommand, Result<TEntity>>
     where TCommand : ICommand<TEntity>;
