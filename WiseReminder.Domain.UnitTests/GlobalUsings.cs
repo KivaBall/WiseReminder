051b408730d8d1de16c1528e3f05ab5b4da5753b
Ihor Kobzar <supermensy@gmail.com>
2024-12-19 03:21:21 +0200

    Revert "Delete WiseReminder.Domain.UnitTests directory"
    This reverts commit 8c7e12088131ec7f4364dbbd6593789ad49bf5ee.

---
+++ WiseReminder.Domain.UnitTests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using FluentAssertions;
+global using WiseReminder.Domain.Authors;
+global using WiseReminder.Domain.Categories;
+global using WiseReminder.Domain.Quotes;
+global using WiseReminder.Domain.Shared;
