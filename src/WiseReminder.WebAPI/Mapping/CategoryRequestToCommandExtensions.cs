220910b690eb9c453011cb15f67ca070be9afc49
Ihor Kobzar <supermensy@gmail.com>
2024-12-19 03:01:44 +0200

    Amend mapping static classes

--- src/WiseReminder.WebAPI/Mapping/CategoryRequestToCommandExtensions.cs
+++ src/WiseReminder.WebAPI/Mapping/CategoryRequestToCommandExtensions.cs
@@ -4,12 +4,6 @@ public static class CategoryRequestToCommandExtensions
 {
     public static CreateCategoryCommand ToCreateCategoryCommand(this BaseCategoryRequest request)
     {
-        if (request.Name == null
-            || request.Description == null)
-        {
-            throw new ArgumentNullException($"{nameof(BaseCategoryRequest)} has null property");
-        }
-
         return new CreateCategoryCommand
         {
             Name = request.Name,
@@ -19,16 +13,9 @@ public static class CategoryRequestToCommandExtensions
 
     public static UpdateCategoryCommand ToUpdateCategoryCommand(this UpdateCategoryRequest request)
     {
-        if (request.Id == null
-            || request.Name == null
-            || request.Description == null)
-        {
-            throw new ArgumentNullException($"{nameof(UpdateCategoryRequest)} has null property");
-        }
-
         return new UpdateCategoryCommand
         {
-            Id = (Guid)request.Id,
+            Id = request.Id,
             Name = request.Name,
             Description = request.Description
         };
