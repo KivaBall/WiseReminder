379a60f25853afd2ad41639e5b8bf11d161a8018
Ihor Kobzar <supermensy@gmail.com>
2024-12-20 23:38:38 +0200

    Add helper classes

---
+++ tests/WiseReminder.WebAPI.IntegrationTests/Abstractions/Helpers/AuthHelper.cs
@@ -0,0 +1,18 @@
+namespace WiseReminder.IntegrationTests.Abstractions.Helpers;
+
+public static class AuthHelper
+{
+    public static async Task LoginAsAdminAsync(this HttpClient httpClient)
+    {
+        var authRequest = new SignInRequest
+        {
+            Login = "wise-reminder-admin-login",
+            Password = "wise-reminder-admin-password"
+        };
+
+        var authResponse = await httpClient.PostAsJsonAsync("api/auth/login-as-admin", authRequest);
+
+        httpClient.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", await authResponse.Content.ReadAsStringAsync());
+    }
+}
