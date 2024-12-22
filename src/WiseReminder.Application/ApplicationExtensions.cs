38fe5e85c2a0c7e1e85525e96d99766596408117
Ihor Kobzar <supermensy@gmail.com>
2024-12-22 23:29:19 +0200

    Add Serilog in extensions of application lyaer

--- src/WiseReminder.Application/ApplicationExtensions.cs
+++ src/WiseReminder.Application/ApplicationExtensions.cs
@@ -2,10 +2,19 @@
 
 public static class ApplicationExtensions
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration configuration)
     {
+        services.AddSerilog(config => config
+            .WriteTo.Console()
+            .WriteTo.Seq(configuration.GetConnectionString("Seq")));
+        
         services.AddMediatR(config =>
-            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+
+            config.AddOpenBehavior(typeof(SerilogPipeline<,>));
+        });
 
         return services;
     }
--- src/WiseReminder.WebAPI/Program.cs
+++ src/WiseReminder.WebAPI/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApplicationServices();
+builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddPresentationServices(builder.Configuration);
 
