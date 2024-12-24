db0546e17dc7fedee2ea6c3ccfc3990e666aef90
KivaBall <supermensy@gmail.com>
2024-12-24 17:41:49 +0200

    Add Username value type

---
+++ src/WiseReminder.Domain/Users/Username.cs
@@ -0,0 +1,11 @@
+﻿namespace WiseReminder.Domain.Users;
+
+public sealed record Username
+{
+    public Username(string value)
+    {
+        Value = value;
+    }
+    
+    public string Value { get; }
+}
