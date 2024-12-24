e70545135b3ec1bce290131e5aeb7890c3f61b9a
Ihor Kobzar <supermensy@gmail.com>
2024-12-24 16:32:15 +0200

    Rename configurations in extensions

--- src/WiseReminder.Application/ApplicationExtensions.cs
+++ src/WiseReminder.Application/ApplicationExtensions.cs
@@ -5,10 +5,17 @@ public static class ApplicationExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services,
         IConfiguration configuration)
     {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.Seq(configuration.GetConnectionString("SeqConnection") ??
+                         throw new NullReferenceException())
+            .CreateLogger();
+
         services.AddSerilog(config => config
             .WriteTo.Console()
-            .WriteTo.Seq(configuration.GetConnectionString("Seq")));
-        
+            .WriteTo.Seq(configuration.GetConnectionString("SeqConnection") ??
+                         throw new NullReferenceException()));
+
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
