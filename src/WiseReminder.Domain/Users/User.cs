17aac6fe62e17c960aa13730ac1b9129ad321b1e
KivaBall <supermensy@gmail.com>
2024-12-24 17:42:44 +0200

    Add User entity

---
+++ src/WiseReminder.Domain/Users/User.cs
@@ -0,0 +1,26 @@
+﻿namespace WiseReminder.Domain.Users;
+
+public sealed class User : Entity<User>
+{
+    public User(Username username, Login login, HashedPassword hashedPassword, SubscriptionType subscriptionType, Author? author)
+    {
+     Username = username;
+     Login = login;
+     HashedPassword = hashedPassword;
+     SubscriptionType = subscriptionType;
+     AuthorId = author?.Id;
+     Author = author;
+    }
+    
+    // ReSharper disable once UnusedMember.Local
+    private User()
+    {
+    }
+
+    public Username Username { get; private set; }
+    public Login Login { get; private set; }
+    public HashedPassword HashedPassword { get; private set; }
+    public SubscriptionType SubscriptionType { get; private set; }
+    public Guid? AuthorId { get; private set; }
+    public Author? Author { get; private set; }
+}
