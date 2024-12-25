761d28424587eea343d1a62c99b5c74c99cf27d6
KivaBall <supermensy@gmail.com>
2024-12-25 17:28:20 +0200

    Add User entity

--- src/WiseReminder.Domain/Users/User.cs
+++ src/WiseReminder.Domain/Users/User.cs
@@ -2,16 +2,17 @@
 
 public sealed class User : Entity<User>
 {
-    public User(Username username, Login login, HashedPassword hashedPassword, SubscriptionType subscriptionType, Author? author)
+    public User(Username username, Login login, HashedPassword hashedPassword,
+        SubscriptionType subscriptionType, Author? author)
     {
-     Username = username;
-     Login = login;
-     HashedPassword = hashedPassword;
-     SubscriptionType = subscriptionType;
-     AuthorId = author?.Id;
-     Author = author;
+        Username = username;
+        Login = login;
+        HashedPassword = hashedPassword;
+        SubscriptionType = subscriptionType;
+        AuthorId = author?.Id;
+        Author = author;
     }
-    
+
     // ReSharper disable once UnusedMember.Local
     private User()
     {
@@ -23,4 +24,30 @@ public sealed class User : Entity<User>
     public SubscriptionType SubscriptionType { get; private set; }
     public Guid? AuthorId { get; private set; }
     public Author? Author { get; private set; }
+
+    public Result<User> ApplySubscription(SubscriptionType subscriptionType)
+    {
+        if (SubscriptionType != SubscriptionType.Free)
+        {
+            return Result.Fail("Subscription has been already applied");
+        }
+
+        SubscriptionType = subscriptionType;
+
+        return Result.Ok(this);
+    }
+
+    public User ChangeUsername(Username username)
+    {
+        Username = username;
+
+        return this;
+    }
+
+    public User ChangePassword(HashedPassword hashedPassword)
+    {
+        HashedPassword = hashedPassword;
+        
+        return this;
+    }
 }
