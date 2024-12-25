469ebcfed65abe24898c3e0aa0d260ae599ac9a6
KivaBall <supermensy@gmail.com>
2024-12-25 18:27:12 +0200

    Add repository for user entity

---
+++ src/WiseReminder.Domain/Users/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace WiseReminder.Domain.Users;
+
+public interface IUserRepository
+{
+    void CreateUser(User user);
+    void UpdateUser(User user);
+    Task DeleteUser(User user);
+    Task<User?> GetUserById(Guid id);
+}
