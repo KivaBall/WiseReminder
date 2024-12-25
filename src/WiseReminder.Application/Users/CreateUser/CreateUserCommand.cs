0ba1ed535d1e5aedd77862b4d6e29715c5d63e61
KivaBall <supermensy@gmail.com>
2024-12-25 18:42:25 +0200

    Add CreateUserCommand

---
+++ src/WiseReminder.Application/Users/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace WiseReminder.Application.Users.CreateUser;
+
+public sealed record CreateUserCommand : ICommand<Guid>
+{
+    public string Username { get; init; }
+    public string Login { get; init; }
+    public string Password { get; init; }
+}
---
+++ src/WiseReminder.Application/Users/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace WiseReminder.Application.Users.CreateUser;
+
+public sealed class CreateUserCommandHandler(
+    IUserRepository userRepository,
+    IUnitOfWork unitOfWork,
+    IEncryptService encryptService) : ICommandHandler<CreateUserCommand, Guid>
+{
+    public async Task<Result<Guid>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var username = new Username(request.Username);
+        var login = new Login(request.Login);
+        var password = new HashedPassword(encryptService.Encrypt(request.Password));
+        
+        var user = new User(username, login, password);
+        
+        userRepository.CreateUser(user);
+        
+        var isSaved = await unitOfWork.SaveChangesAsync();
+
+        if (isSaved.IsFailed)
+        {
+            return Result.Fail(isSaved.Errors);
+        }
+
+        return Result.Ok(user.Id);
+    }
+}
