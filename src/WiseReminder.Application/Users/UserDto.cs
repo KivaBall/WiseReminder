470951dbb699971fb061b2d3250312acbebc6733
Ihor Kobzar <supermensy@gmail.com>
2024-12-26 13:55:19 +0200

    Add UserDto

---
+++ src/WiseReminder.Application/Users/UserDto.cs
@@ -0,0 +1,10 @@
+namespace WiseReminder.Application.Users;
+
+public sealed record UserDto
+{
+    public required Guid Id { get; init; }
+    public required string Username { get; init; }
+    public required string Login { get; init; }
+    public required Subscription Subscription { get; init; }
+    public required Guid? AuthorId { get; init; }
+};
