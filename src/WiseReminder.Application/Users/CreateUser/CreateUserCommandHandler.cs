81326d36903c94f6b29d40f871a3bacb57615687
Ihor Kobzar <supermensy@gmail.com>
2024-12-26 13:59:53 +0200

    Add CreateUser and DeleteUser commands

--- src/WiseReminder.Application/Users/CreateUser/CreateUserCommandHandler.cs
+++ src/WiseReminder.Application/Users/CreateUser/CreateUserCommandHandler.cs
@@ -3,12 +3,17 @@
 public sealed class CreateUserCommandHandler(
     IUserRepository userRepository,
     IUnitOfWork unitOfWork,
-    IEncryptService encryptService) : ICommandHandler<CreateUserCommand, Guid>
+    IEncryptService encryptService)
+    : ICommandHandler<CreateUserCommand, Guid>
 {
-    public async Task<Result<Guid>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    public async Task<Result<Guid>> Handle(
+        CreateUserCommand request, 
+        CancellationToken cancellationToken)
     {
         var username = new Username(request.Username);
+        
         var login = new Login(request.Login);
+        
         var password = new HashedPassword(encryptService.Encrypt(request.Password));
         
         var user = new User(username, login, password);
@@ -17,11 +22,6 @@ public sealed class CreateUserCommandHandler(
         
         var isSaved = await unitOfWork.SaveChangesAsync();
 
-        if (isSaved.IsFailed)
-        {
-            return Result.Fail(isSaved.Errors);
-        }
-
-        return Result.Ok(user.Id);
+        return isSaved.IsFailed ? Result.Fail(isSaved.Errors) : Result.Ok(user.Id);
     }
 }
