b46f9de0ec54352f7a19b111d7e92a0bd3d40863
Ihor Kobzar <supermensy@gmail.com>
2024-12-26 13:51:53 +0200

    Rebuild mappers

--- src/WiseReminder.Application/Abstractions/Mapping/AuthorToDtoMapper.cs
+++ src/WiseReminder.Application/Abstractions/Mapping/AuthorToDtoMapper.cs
@@ -9,11 +9,8 @@ public static class AuthorToDtoMapper
             Id = author.Id,
             Name = author.Name.Value,
             Biography = author.Biography.Value,
-            DateOfBirth = new DateOnly(author.BirthDate.Year, author.BirthDate.Month,
-                author.BirthDate.Day),
-            DateOfDeath = author.DeathDate == null
-                ? null
-                : new DateOnly(author.DeathDate.Year, author.DeathDate.Month, author.DeathDate.Day),
+            BirthDate = author.BirthDate.Value,
+            DeathDate = author.DeathDate?.Value,
             UserId = author.UserId
         };
     }
