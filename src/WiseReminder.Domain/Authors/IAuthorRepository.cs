7db1bab3f76fff4b171541e824c042d471497d79
Ihor Kobzar <supermensy@gmail.com>
2024-12-26 13:30:09 +0200

    Remove todo comments to amend it right now

--- src/WiseReminder.Domain/Authors/IAuthorRepository.cs
+++ src/WiseReminder.Domain/Authors/IAuthorRepository.cs
@@ -4,7 +4,7 @@ public interface IAuthorRepository
 {
     void CreateAuthor(Author author);
     void UpdateAuthor(Author author);
-    Task DeleteAuthor(Author author); //TODO: deletion must not be Task!
+    void DeleteAuthor(Author author);
     Task<Author?> GetAuthorById(Guid id);
     Task<ICollection<Author>> GetAllAuthors();
 }
