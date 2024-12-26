fdef516b1ef798930ec89c22a2c812aa6b0c9bb6
Ihor Kobzar <supermensy@gmail.com>
2024-12-26 03:45:56 +0200

    Rebuild Date value object

--- src/WiseReminder.Domain/Shared/Date.cs
+++ src/WiseReminder.Domain/Shared/Date.cs
@@ -2,44 +2,24 @@
 
 public sealed record Date
 {
-    private Date(short year, byte month, byte day)
+    private Date(DateOnly value)
     {
-        Year = year;
-        Month = month;
-        Day = day;
+        Value = value;
     }
 
-    public short Year { get; }
-    public byte Month { get; }
-    public byte Day { get; }
+    public DateOnly Value { get; }
 
-    public static Result<Date> Create(short year, byte month, byte day)
+    public static Result<Date> Create(DateOnly dateStruct)
     {
-        const short minYear = 1;
         const short maxYear = 2048;
-
-        if (year < minYear || year > maxYear)
+        
+        if (dateStruct.Year > maxYear)
         {
-            return Result.Fail("Year must be between 1 and the current one");
+            return Result.Fail(DateErrors.YearOutOfRange);
         }
 
-        const byte minMonth = 1;
-        const byte maxMonth = 12;
-
-        if (month < minMonth || month > maxMonth)
-        {
-            return Result.Fail("Month must be between 1 and 12");
-        }
-
-        const byte minDay = 1;
-
-        if (day < minDay || day > DateTime.DaysInMonth(year, month))
-        {
-            return Result.Fail("Day must be between 1 and 31");
-        }
-
-        var date = new Date(year, month, day);
-
+        var date = new Date(dateStruct);
+        
         return Result.Ok(date);
     }
 }
