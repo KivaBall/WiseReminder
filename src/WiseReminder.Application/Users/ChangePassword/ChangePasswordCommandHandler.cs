bde326bcd0b18ae7635fc8bc180e18341960d8c9
Ihor Kobzar <supermensy@gmail.com>
2024-12-26 14:00:12 +0200

    Add abilities to change password or username

---
+++ src/WiseReminder.Application/Users/ChangePassword/ChangePasswordCommandHandler.cs
@@ -0,0 +1,42 @@
+namespace WiseReminder.Application.Users.ChangePassword;
+
+public sealed class ChangePasswordCommandHandler(
+    IUserRepository userRepository,
+    ISender sender,
+    IEncryptService encryptService,
+    IUnitOfWork unitOfWork)
+    : ICommandHandler<ChangePasswordCommand>
+{
+    public async Task<Result> Handle(
+        ChangePasswordCommand request,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetUserByIdQuery { Id = request.Id };
+
+        var user = await sender.Send(query);
+
+        if (user.IsFailed)
+        {
+            return Result.Fail(UserErrors.UserNotFound);
+        }
+
+        var isOldPasswordCorrect = encryptService.Check(
+            user.Value.HashedPassword.Value,
+            request.OldPassword);
+
+        if (!isOldPasswordCorrect)
+        {
+            return Result.Fail(UserErrors.PasswordNotCorrect);
+        }
+        
+        var newPassword = new HashedPassword(encryptService.Encrypt(request.NewPassword));
+
+        user.Value.ChangePassword(newPassword);
+
+        userRepository.UpdateUser(user.Value);
+
+        var isSaved = await unitOfWork.SaveChangesAsync();
+
+        return isSaved.IsFailed ? Result.Fail(isSaved.Errors) : Result.Ok();
+    }
+}
