a1a8e1d8b581dc10d2cdfc3b937ad88e0d5136df
Ihor Kobzar <supermensy@gmail.com>
2024-12-26 13:51:18 +0200

    Put void instead of Task in IUserRepository

--- src/WiseReminder.Domain/Users/IUserRepository.cs
+++ src/WiseReminder.Domain/Users/IUserRepository.cs
@@ -4,6 +4,6 @@ public interface IUserRepository
 {
     void CreateUser(User user);
     void UpdateUser(User user);
-    Task DeleteUser(User user);
+    void DeleteUser(User user);
     Task<User?> GetUserById(Guid id);
 }
