89484a0f051ce20ef7c1a7a981833d5109d2b0ad
Ihor Kobzar <supermensy@gmail.com>
2024-12-26 03:47:50 +0200

    Remove unnecessary rows in application extensions

--- src/WiseReminder.Application/ApplicationExtensions.cs
+++ src/WiseReminder.Application/ApplicationExtensions.cs
@@ -2,27 +2,17 @@
 
 public static class ApplicationExtensions
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+    public static void AddApplicationServices(this IServiceCollection services,
         IConfiguration configuration)
     {
-        Log.Logger = new LoggerConfiguration()
-            .WriteTo.Console()
-            .WriteTo.Seq(configuration.GetConnectionString("SeqConnection") ??
-                         throw new NullReferenceException())
-            .CreateLogger();
-
         services.AddSerilog(config => config
             .WriteTo.Console()
-            .WriteTo.Seq(configuration.GetConnectionString("SeqConnection") ??
-                         throw new NullReferenceException()));
+            .WriteTo.Seq(configuration.GetConnectionString("SeqConnection")!));
 
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-
             config.AddOpenBehavior(typeof(SerilogPipeline<,>));
         });
-
-        return services;
     }
 }
