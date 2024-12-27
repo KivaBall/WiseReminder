2c38cd178206c7af684eb701b02207876a14ce9e
Ihor Kobzar <supermensy@gmail.com>
2024-12-27 19:18:19 +0200

    Add UpdateQuoteAsUser

---
+++ src/WiseReminder.Application/Quotes/UpdateQuoteAsUser/UpdateQuoteAsUserCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace WiseReminder.Application.Quotes.UpdateQuote;
+
+public sealed record UpdateQuoteAsUserCommand : ICommand
+{
+    public required Guid Id { get; init; }
+    public required string Text { get; init; }
+    public required Guid AuthorId { get; init; }
+    public required Guid CategoryId { get; init; }
+    public required DateOnly QuoteDate { get; init; }
+    public required Guid UserId { get; init; }
+}
---
+++ src/WiseReminder.Application/Quotes/UpdateQuoteAsUser/UpdateQuoteAsUserCommandHandler.cs
@@ -0,0 +1,67 @@
+﻿namespace WiseReminder.Application.Quotes.UpdateQuote;
+
+public sealed class UpdateQuoteAsUserCommandHandler(
+    IQuoteRepository quoteRepository,
+    IUnitOfWork unitOfWork,
+    ISender sender)
+    : ICommandHandler<UpdateQuoteAsUserCommand>
+{
+    public async Task<Result> Handle(
+        UpdateQuoteAsUserCommand request,
+        CancellationToken cancellationToken)
+    {
+        var quoteQuery = new GetQuoteByIdQuery { Id = request.Id };
+
+        var quote = await sender.Send(quoteQuery, cancellationToken);
+
+        if (quote.IsFailed)
+        {
+            return Result.Fail(quote.Errors);
+        }
+
+        var authorQuery = new GetAuthorByIdQuery { Id = request.AuthorId };
+
+        var author = await sender.Send(authorQuery, cancellationToken);
+
+        if (author.IsFailed)
+        {
+            return Result.Fail(author.Errors);
+        }
+
+        if (author.Value.UserId == null)
+        {
+            return Result.Fail(AuthorErrors.AuthorNotExistsForUser);
+        }
+
+        if (author.Value.UserId != request.UserId)
+        {
+            return Result.Fail(UserErrors.UserIdNotValid);
+        }
+        
+        var categoryQuery = new GetCategoryByIdQuery { Id = request.CategoryId };
+
+        var category = await sender.Send(categoryQuery, cancellationToken);
+
+        if (category.IsFailed)
+        {
+            return Result.Fail(category.Errors);
+        }
+
+        var text = new Text(request.Text);
+
+        var quoteDate = Date.Create(request.QuoteDate);
+
+        if (quoteDate.IsFailed)
+        {
+            return Result.Fail(quoteDate.Errors);
+        }
+
+        quote.Value.Update(text, author.Value, category.Value, quoteDate.Value);
+
+        quoteRepository.UpdateQuote(quote.Value);
+
+        var isSaved = await unitOfWork.SaveChangesAsync();
+
+        return isSaved.IsFailed ? Result.Fail(isSaved.Errors) : Result.Ok();
+    }
+}
