3fa61ec4282a371f1a6ec12cd16e3c8c28f75f6c
Ihor Kobzar <supermensy@gmail.com>
2024-12-27 19:18:03 +0200

    Add DeleteQuoteAsUser

---
+++ src/WiseReminder.Application/Quotes/DeleteQuoteAsUser/DeleteQuoteAsUserCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace WiseReminder.Application.Quotes.DeleteQuote;
+
+public sealed record DeleteQuoteAsUserCommand : ICommand
+{
+    public required Guid Id { get; init; }
+    public required Guid UserId { get; init; }
+}
---
+++ src/WiseReminder.Application/Quotes/DeleteQuoteAsUser/DeleteQuoteAsUserCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿namespace WiseReminder.Application.Quotes.DeleteQuote;
+
+public sealed class DeleteQuoteAsUserCommandHandler(
+    IQuoteRepository quoteRepository,
+    IUnitOfWork unitOfWork,
+    ISender sender)
+    : ICommandHandler<DeleteQuoteAsUserCommand>
+{
+    public async Task<Result> Handle(
+        DeleteQuoteAsUserCommand request,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetQuoteByIdQuery { Id = request.Id };
+
+        var quote = await sender.Send(query, cancellationToken);
+
+        if (quote.IsFailed)
+        {
+            return Result.Fail(quote.Errors);
+        }
+
+        var authorQuery = new GetAuthorByIdQuery{Id = quote.Value.AuthorId};
+        
+        var author = await sender.Send(authorQuery, cancellationToken);
+
+        if (author.IsFailed)
+        {
+            return Result.Fail(author.Errors);
+        }
+
+        if (author.Value.UserId == null)
+        {
+            return Result.Fail(AuthorErrors.AuthorNotExistsForUser);
+        }
+
+        if (author.Value.UserId != request.UserId)
+        {
+            return Result.Fail(UserErrors.UserIdNotValid);
+        }
+
+        quoteRepository.DeleteQuote(quote.Value);
+
+        var isSaved = await unitOfWork.SaveChangesAsync();
+
+        return isSaved.IsFailed ? Result.Fail(isSaved.Errors) : Result.Ok();
+    }
+}
