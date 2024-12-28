7508721dde7a0e887b36b9ebccc01d0f71dc8118
Ihor Kobzar <supermensy@gmail.com>
2024-12-28 03:26:59 +0200

    Add new method for user repository

--- src/WiseReminder.Domain/Users/IUserRepository.cs
+++ src/WiseReminder.Domain/Users/IUserRepository.cs
@@ -6,4 +6,5 @@ public interface IUserRepository
     void UpdateUser(User user);
     void DeleteUser(User user);
     Task<User?> GetUserById(Guid id);
+    Task<User?> GetUserByLogin(Login login);
 }
--- src/WiseReminder.Infrastructure/Repositories/UserRepository.cs
+++ src/WiseReminder.Infrastructure/Repositories/UserRepository.cs
@@ -21,9 +21,15 @@ public sealed class UserRepository(
         context.Update(user);
     }
 
-    public Task<User?> GetUserById(Guid id)
+    public async Task<User?> GetUserById(Guid id)
     {
-        return context.Users
+        return await context.Users
             .FirstOrDefaultAsync(u => u.Id == id);
+    }
+
+    public async Task<User?> GetUserByLogin(Login login)
+    {
+        return await context.Users
+            .FirstOrDefaultAsync(u => u.Login == login);
     }
 }
