8572da18b77ef0c49fab686ee5e5fac9191babf5
Ihor Kobzar <supermensy@gmail.com>
2024-12-29 03:36:53 +0200

    Return string instead of number for Subscription

--- src/WiseReminder.Application/Users/UserDto.cs
+++ src/WiseReminder.Application/Users/UserDto.cs
@@ -5,6 +5,6 @@ public sealed record UserDto
     public required Guid Id { get; init; }
     public required string Username { get; init; }
     public required string Login { get; init; }
-    public required Subscription Subscription { get; init; }
+    public required string Subscription { get; init; }
     public required Guid? AuthorId { get; init; }
 }
