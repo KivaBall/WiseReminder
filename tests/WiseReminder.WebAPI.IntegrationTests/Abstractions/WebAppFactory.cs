93ca87785053c37007daf2948af1486e9955a229
Ihor Kobzar <supermensy@gmail.com>
2025-01-02 23:11:15 +0200

    Rebuild WebAppFactory for test containers instead of fake cache and inmemory db

--- tests/WiseReminder.WebAPI.IntegrationTests/Abstractions/WebAppFactory.cs
+++ tests/WiseReminder.WebAPI.IntegrationTests/Abstractions/WebAppFactory.cs
@@ -2,18 +2,41 @@ namespace WiseReminder.IntegrationTests.Abstractions;
 
 public sealed class WebAppFactory : WebApplicationFactory<Program>
 {
+    private readonly PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithName($"test-postgres-{Guid.NewGuid()}")
+        .WithUsername("postgres")
+        .WithPassword("123456")
+        .WithDatabase("wise_reminder")
+        .Build();
+
+    private readonly RedisContainer _redisContainer = new RedisBuilder()
+        .WithImage("redis:latest")
+        .WithName($"test-redis-{Guid.NewGuid()}")
+        .Build();
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.ConfigureTestServices(services =>
+        var redisTask = _redisContainer.StartAsync();
+        var postgresqlTask = _postgreSqlContainer.StartAsync();
+
+        Task.WaitAll(redisTask, postgresqlTask);
+
+        builder.ConfigureAppConfiguration(config =>
         {
-            services.RemoveAll<ICacheService>();
-            services.AddSingleton<ICacheService, FakeCacheService>();
-
-            services.RemoveAll<IMemoryCache>();
-            services.AddSingleton<IMemoryCache, FakeMemoryCache>();
-
-            services.RemoveAll<DbContextOptions<AppDbContext>>();
-            services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("TestDb"));
+            config.AddInMemoryCollection(new Dictionary<string, string>
+            {
+                ["ConnectionStrings:RedisConnection"] = _redisContainer.GetConnectionString(),
+                ["ConnectionStrings:DbConnection"] = _postgreSqlContainer.GetConnectionString()
+            }!);
         });
+    }
+
+    public override async ValueTask DisposeAsync()
+    {
+        await _redisContainer.StopAsync();
+        await _postgreSqlContainer.StopAsync();
+
+        await base.DisposeAsync();
     }
 }
