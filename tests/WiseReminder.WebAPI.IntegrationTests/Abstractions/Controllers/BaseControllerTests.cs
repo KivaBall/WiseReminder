6439fb8432115e22fdb7446e6036b5e1528c7eff
Ihor Kobzar <supermensy@gmail.com>
2025-01-05 15:38:45 +0200

    Rebuild BaseControllerTests.cs

--- tests/WiseReminder.WebAPI.IntegrationTests/Abstractions/Controllers/BaseControllerTests.cs
+++ tests/WiseReminder.WebAPI.IntegrationTests/Abstractions/Controllers/BaseControllerTests.cs
@@ -1,15 +1,30 @@
 namespace WiseReminder.IntegrationTests.Abstractions.Controllers;
 
-public abstract class BaseControllerTests : IDisposable
+public abstract class BaseControllerTests : IAsyncLifetime
 {
+    private readonly WebAppFactory _factory;
+
     protected BaseControllerTests()
     {
-        Client = new WebAppFactory().CreateClient();
-        Ids = Client.SeedDataAsync().Result;
+        _factory = new WebAppFactory();
+        Client = _factory.CreateClient();
     }
 
     protected HttpClient Client { get; }
-    protected IdsFixture Ids { get; }
+    protected AdminIdsFixture AdminIds { get; private set; } = null!;
+    protected UserIdsFixture UserIds { get; private set; } = null!;
+
+    public async Task InitializeAsync()
+    {
+        AdminIds = await Client.SeedAdminDataAsync();
+        UserIds = await Client.SeedUserWithDataAsync(AdminIds.CategoryId);
+    }
+
+    public async Task DisposeAsync()
+    {
+        Client.Dispose();
+        await _factory.DisposeAsync();
+    }
 
     public void Dispose()
     {
