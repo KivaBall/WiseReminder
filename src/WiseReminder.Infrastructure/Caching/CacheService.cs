21e895bd6f08344a2fb75944925c14b13aad9c52
Ihor Kobzar <supermensy@gmail.com>
2025-01-05 16:23:33 +0200

    Rebuild cache system and add quote converter (will rebuild this layer)

--- src/WiseReminder.Infrastructure/Caching/CacheService.cs
+++ src/WiseReminder.Infrastructure/Caching/CacheService.cs
@@ -1,34 +1,60 @@
 ﻿namespace WiseReminder.Infrastructure.Caching;
 
-public sealed class CacheService(IDistributedCache distributedCache) : ICacheService
+public sealed class CacheService(IDistributedCache cache) : ICacheService
 {
-    public async Task CreateAsync<T>(string key, T entity, TimeSpan? time = null) where T : notnull
+    public async Task<T> GetOrCreateAsync<T>(
+        string key,
+        Func<Task<T?>> entityFunc,
+        TimeSpan? expiration = null)
     {
-        var obj = JsonConvert.SerializeObject(entity);
+        var serializedObject = await cache.GetStringAsync(key);
+
+        if (serializedObject != null)
+        {
+            var deserializedObject =
+                JsonConvert.DeserializeObject<T>(serializedObject, new QuoteConverter());
+
+            if (deserializedObject == null)
+            {
+                throw new Exception($"Could not deserialize object of type {typeof(T).Name}");
+            }
+
+            return deserializedObject;
+        }
+
+        var entity = await entityFunc();
+
+        if (entity == null)
+        {
+            return entity!;
+        }
+
+        var createdSerializedObject = JsonConvert.SerializeObject(entity, new QuoteConverter());
 
         var options = new DistributedCacheEntryOptions
         {
-            AbsoluteExpirationRelativeToNow = time ?? TimeSpan.FromMinutes(2)
+            AbsoluteExpirationRelativeToNow = expiration ?? TimeSpan.FromMinutes(5)
         };
 
-        await distributedCache.SetStringAsync(key, obj, options);
+        await cache.SetStringAsync(key, createdSerializedObject, options);
+
+        return entity;
     }
 
-    public async Task<T?> GetAsync<T>(string key) where T : class
+    public async Task SetAsync<T>(string key, T entity)
     {
-        var obj = await distributedCache.GetStringAsync(key);
+        var createdSerializedObject = JsonConvert.SerializeObject(entity, new QuoteConverter());
 
-        if (obj == null)
+        var options = new DistributedCacheEntryOptions
         {
-            return null;
-        }
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+        };
 
-        var deserializedObj = JsonConvert.DeserializeObject<T>(obj, new JsonSerializerSettings
-        {
-            ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
-            ContractResolver = new PrivateResolver()
-        });
+        await cache.SetStringAsync(key, createdSerializedObject, options);
+    }
 
-        return deserializedObj;
+    public async Task RemoveAsync(string key)
+    {
+        await cache.RemoveAsync(key);
     }
 }
