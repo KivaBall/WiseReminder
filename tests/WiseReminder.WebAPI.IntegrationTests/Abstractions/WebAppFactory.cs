371edca216897c3928ecec7c533f7609cea98fe9
Ihor Kobzar <supermensy@gmail.com>
2025-01-05 15:30:50 +0200

    Speed up web app factory with alpine versions of postgresql and redis

--- tests/WiseReminder.WebAPI.IntegrationTests/Abstractions/WebAppFactory.cs
+++ tests/WiseReminder.WebAPI.IntegrationTests/Abstractions/WebAppFactory.cs
@@ -1,17 +1,17 @@
 namespace WiseReminder.IntegrationTests.Abstractions;
 
-public sealed class WebAppFactory : WebApplicationFactory<Program>
+public sealed class WebAppFactory : WebApplicationFactory<Program>, IDisposable
 {
     private readonly PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder()
-        .WithImage("postgres:latest")
+        .WithImage("postgres:alpine")
         .WithName($"test-postgres-{Guid.NewGuid()}")
         .WithUsername("postgres")
         .WithPassword("123456")
-        .WithDatabase("wise_reminder")
+        .WithDatabase("wisereminder")
         .Build();
 
     private readonly RedisContainer _redisContainer = new RedisBuilder()
-        .WithImage("redis:latest")
+        .WithImage("redis:alpine")
         .WithName($"test-redis-{Guid.NewGuid()}")
         .Build();
 
@@ -22,14 +22,40 @@ public sealed class WebAppFactory : WebApplicationFactory<Program>
 
         Task.WaitAll(redisTask, postgresqlTask);
 
+        EnsurePostgresConnection();
+
         builder.ConfigureAppConfiguration(config =>
         {
             config.AddInMemoryCollection(new Dictionary<string, string>
             {
                 ["ConnectionStrings:RedisConnection"] = _redisContainer.GetConnectionString(),
-                ["ConnectionStrings:DbConnection"] = _postgreSqlContainer.GetConnectionString()
+                ["ConnectionStrings:DbConnection"] = _postgreSqlContainer.GetConnectionString(),
+                ["InitialSeeding"] = "false",
+                ["AllowScalar"] = "false"
             }!);
         });
+    }
+
+    private void EnsurePostgresConnection()
+    {
+        for (var i = 1; i <= 40; i++)
+        {
+            try
+            {
+                using var connection =
+                    new NpgsqlConnection(_postgreSqlContainer.GetConnectionString());
+
+                connection.Open();
+
+                return;
+            }
+            catch (Exception ex)
+            {
+                Thread.Sleep(250);
+            }
+        }
+
+        throw new Exception("No connection established");
     }
 
     public override async ValueTask DisposeAsync()
