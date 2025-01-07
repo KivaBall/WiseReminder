f3641919ecd6744d19f3c632f0e88ddaafdbaa2f
Ihor Kobzar <supermensy@gmail.com>
2025-01-07 15:25:44 +0200

    Remove unnecessary global usings and add mediatr one

--- src/WiseReminder.Domain/GlobalUsings.cs
+++ src/WiseReminder.Domain/GlobalUsings.cs
@@ -1,7 +1,6 @@
 global using FluentResults;
+global using MediatR;
 global using WiseReminder.Domain.Abstractions;
 global using WiseReminder.Domain.Authors;
-global using WiseReminder.Domain.Categories;
-global using WiseReminder.Domain.Quotes;
 global using WiseReminder.Domain.Shared;
 global using WiseReminder.Domain.Users;
