fec8626a68f56b421d4acf5441604042e6b27b37
Ihor Kobzar <supermensy@gmail.com>
2025-01-08 23:49:39 +0200

    Add Reaction entity with finished business logic

---
+++ src/WiseReminder.Domain/Reactions/Reaction.cs
@@ -0,0 +1,27 @@
+namespace WiseReminder.Domain.Reactions;
+
+public sealed class Reaction : Entity<Reaction>
+{
+    public Reaction(Guid quoteId, Guid userId, IsLike isLike)
+    {
+        QuoteId = quoteId;
+        UserId = userId;
+        IsLike = isLike;
+    }
+
+    public Guid QuoteId { get; private set; }
+    public Guid UserId { get; private set; }
+    public IsLike IsLike { get; private set; }
+
+    public Result Update(IsLike isLike)
+    {
+        if (IsLike == isLike)
+        {
+            return ReactionErrors.TheSameReaction;
+        }
+
+        IsLike = isLike;
+
+        return Result.Ok();
+    }
+}
