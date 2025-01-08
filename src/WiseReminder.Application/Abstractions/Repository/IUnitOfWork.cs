164aaafc2c5a0a0c5b78a1eb5a00cc08caefdb40
Ihor Kobzar <supermensy@gmail.com>
2025-01-09 00:18:23 +0200

    Rename SaveChangesAsyncWithResult in IUnitOfWork to two words and implement cancellation token them

--- src/WiseReminder.Application/Abstractions/Repository/IUnitOfWork.cs
+++ src/WiseReminder.Application/Abstractions/Repository/IUnitOfWork.cs
@@ -2,6 +2,8 @@
 
 public interface IUnitOfWork
 {
-    Task<Result> SaveChangesAsync();
-    Task<Result<TResult>> SaveChangesAsyncWithResult<TResult>(Func<TResult> entity);
+    Task<Result> SaveChangesAsync(CancellationToken cancellationToken);
+
+    Task<Result<TResult>> SaveChangesAsync<TResult>(Func<TResult> entity,
+        CancellationToken cancellationToken);
 }
