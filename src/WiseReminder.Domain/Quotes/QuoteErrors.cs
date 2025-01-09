a15d2e1defafb20893f6070de62ee159e44ac3bb
Ihor Kobzar <supermensy@gmail.com>
2025-01-09 23:46:13 +0200

    Rename and decorate methods for business logic of quotes

--- src/WiseReminder.Domain/Quotes/QuoteErrors.cs
+++ src/WiseReminder.Domain/Quotes/QuoteErrors.cs
@@ -6,8 +6,8 @@ public static class QuoteErrors
         new Error("The quote with the specified ID was not found");
 
     public static Result QuoteDateOutOfRange =>
-        new Error("The quote date is outside the" +
-                  " allowed range based on the author's birth and death dates");
+        new Error("The quote date is outside the " +
+                  "allowed range based on the author's birth and death dates");
 
     public static Result QuoteLimitExceeded =>
         new Error("The author has exceeded the allowed number of quotes for their subscription");
