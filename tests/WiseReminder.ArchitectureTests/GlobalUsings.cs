da48cf2f39709ca516cc0a7297338257c40fa0c1
Ihor Kobzar <supermensy@gmail.com>
2025-01-11 23:11:18 +0200

    Rebuild architecture tests

--- tests/WiseReminder.ArchitectureTests/GlobalUsings.cs
+++ tests/WiseReminder.ArchitectureTests/GlobalUsings.cs
@@ -1,5 +1,8 @@
 global using WiseReminder.Application;
 global using WiseReminder.Domain.Abstractions;
+global using WiseReminder.Domain.UnitTests.Data;
 global using WiseReminder.Infrastructure;
+global using WiseReminder.IntegrationTests.Abstractions;
 global using WiseReminder.WebAPI;
+global using Xunit;
 global using Xunit.Abstractions;
